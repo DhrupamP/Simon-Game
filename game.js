73acd6c676b10083c3cbcf01e5a539073156ac20
DhrupamP <63700466+DhrupamP@users.noreply.github.com>
2021-07-13 12:53:52 +0400

    edit game.js

--- game.js
+++ game.js
@@ -1,100 +1,3 @@
-
-// var gamePattern = [];
-// var buttonColors = ["red", "blue", "green", "yellow"];
-// var userClickedPattern = [];
-// var level = 0;
-// var started = false;
-
-
-// function playSound(name) {
-//     var audio = new Audio("sounds/" + name + ".mp3");
-//     audio.play();
-
-// }
-
-// $(".btn").click(function () {
-       
-//     var userChosenColor = $(this).attr("id");
-//     userClickedPattern.push(userChosenColor);
-//     playSound(userChosenColor);
-//     animatePress(userChosenColor);
-    
-//     checkAnswer(userClickedPattern.length - 1);
-
-//     });
-
-
-// function nextSequence() {
-
-//     userClickedPattern = [];
-
-//     level++;
-//     $("h1").text("level "+level);
-//     var randomnumber = Math.floor(Math.random() * 4);
-//     var randomChosenColor = buttonColors[randomnumber];
-//     gamePattern.push(randomChosenColor);
-//     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-//     playSound(randomChosenColor);
-    
-    
-    
-// }
-
-// function animatePress(currentColor) {
-//     $("#" + currentColor).addClass(" pressed");
-//     setTimeout(function () { $("#" + currentColor).removeClass(" pressed") }, 100);
-// }
-
-
-// $(document).keypress(function() {
-//   if (!started) {
-//     $("#level-title").text("Level 0");
-//     nextSequence();
-//     started = true;
-//   }
-// });
-    
-// function checkAnswer(currentLevel) {
-
-//     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-//       console.log("success");
-
-//       if (userClickedPattern.length === gamePattern.length){
-
-//         setTimeout(function () {
-//           nextSequence();
-//         }, 1000);
-
-//       }
-
-//     } else {
-
-//         console.log("wrong");
-//         var wrongaudio = new Audio("sounds/wrong.mp3")
-//         wrongaudio.play();
-//         $("body").addClass("game-over");
-//         setTimeout(function () {
-//             $("body").removeClass("game-over")
-//         }, 200);
-//         $("h1").text("Game Over, Press any Key to Restart");
-
-//         startOver();
-
-//     }
-
-// }
-
-// function startOver() {
-//     level = 0;
-//     gamePattern = [];
-//     started = false;
-//     // $("h1").text("Press A Key To Start");
-// }
-    
-
-
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -148,7 +51,7 @@ function checkAnswer(currentLevel) {
 
       $("#level-title").text("Game Over, Press Any Key to Restart");
 
-      //2. Call startOver() if the user gets the sequence wrong.
+     
       startOver();
     }
 
@@ -179,11 +82,8 @@ function animatePress(currentColor) {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
-
-//1. Create a new function called startOver().
 function startOver() {
 
-  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
   level = 0;
   gamePattern = [];
   started = false;
